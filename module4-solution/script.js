82988ea8c82d4e6d04db494efd02d02e9f3aa233
Farhan <nawaz.farhan26@gmail.com>
2021-05-04 21:19:45 +0530

    fifth commit

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -33,7 +33,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function(window){
+(function(){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -62,4 +62,4 @@ for (var i=0;i<names.length;i++/* fill in parts of the 'for' loop to loop over n
     helloSpeaker.speak(names[i]);// helloSpeaker.xxxx
   }
 }
-})(window);
+})();
